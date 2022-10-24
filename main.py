ab021982dea5bc062c06997bdf0d2cbe6cf19262
Daniil Peleschei <maindan444@mail.ru>
2022-10-24 17:03:21 +0200

    push1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-a = 10
+
+from bs4 import BeautifulSoup
+
+with open("blank/index.html", encoding="UTF-8") as file:
+    src = file.read()
+# print(src)
+
+soup = BeautifulSoup(src, "lxml")
+
+title = soup.title
+print(title.text)
